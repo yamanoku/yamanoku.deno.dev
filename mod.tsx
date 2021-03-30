e0b667fb8c7ef8a224bbfc01a14836d87770e30d
yamanoku <0910yama@gmail.com>
2021-03-30 22:01:22 +0900

    init

---
+++ mod.tsx
@@ -0,0 +1,41 @@
+/* @jsx h */
+import {
+  h,
+  renderHTML,
+} from "https://denopkg.com/syumai/deno-libs/jsx/renderer.ts";
+
+const Body = () => (
+  <body>
+    <main>
+      <h1>yamanoku.deno.dev</h1>
+      <a href="https://github.com/yamanoku/yamanoku.deno.dev" target="_blank" rel="noopener">GitHub Source</a>
+    </main>
+  </body>
+);
+
+const html = (
+  <html>
+    <head>
+      <title>yamanoku.deno.dev</title>
+      <link rel="stylesheet" href="https://unpkg.com/yama-normalize@0.0.11/yama-normalize.css" />
+    </head>
+    <Body />
+  </html>
+);
+
+interface Responder {
+  respondWith(res: Response): void;
+}
+
+addEventListener("fetch", (event) => {
+  const e = (event as unknown) as Responder;
+  e.respondWith(
+    new Response(renderHTML(html), {
+      status: 200,
+      headers: {
+        server: "denosr",
+        "content-type": "text/html",
+      },
+    })
+  );
+});
